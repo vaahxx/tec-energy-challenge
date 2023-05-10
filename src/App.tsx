54a8ab837827bc8e9d026ab391465a95af267f20
Valentina Garavaglia <vaahxx@gmail.com>
2023-05-10 13:07:30 -0400

    create ProductionGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { Fragment, useState } from "react";
-import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
-import {
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChartPieIcon, CursorArrowRaysIcon } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import ProductionGrid from "./components/ProductionGrid";
 
 const products = [
   {
@@ -29,83 +25,97 @@ function classNames(...classes) {
 
 export default function App() {
   return (
-    <header className='bg-white'>
-      <nav
-        className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
-        aria-label='Global'
-      >
-        <div className='flex lg:flex-1'>
-          <a href='#' className='-m-1.5 p-1.5'>
-            <span className='sr-only'>Your Company</span>
-            <img
-              className='h-8 w-auto'
-              src='https://www.tecenergy.ca/favicon.ico'
-              alt=''
-            />
-          </a>
-        </div>
-        <Popover.Group className='hidden lg:flex lg:gap-x-12'>
-          <Popover className='relative'>
-            <Popover.Button className='flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900'>
-              Grid View
-              <ChevronDownIcon
-                className='h-5 w-5 flex-none text-gray-400'
-                aria-hidden='true'
+    <>
+      <header className='bg-white'>
+        <nav
+          className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
+          aria-label='Global'
+        >
+          <div className='flex lg:flex-1'>
+            <a href='#' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Your Company</span>
+              <img
+                className='h-8 w-auto'
+                src='https://www.tecenergy.ca/favicon.ico'
+                alt=''
               />
-            </Popover.Button>
+            </a>
+          </div>
+          <Popover.Group className='hidden lg:flex lg:gap-x-12'>
+            <Popover className='relative'>
+              <Popover.Button className='flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900'>
+                Grid View
+                <ChevronDownIcon
+                  className='h-5 w-5 flex-none text-gray-400'
+                  aria-hidden='true'
+                />
+              </Popover.Button>
 
-            <Transition
-              as={Fragment}
-              enter='transition ease-out duration-200'
-              enterFrom='opacity-0 translate-y-1'
-              enterTo='opacity-100 translate-y-0'
-              leave='transition ease-in duration-150'
-              leaveFrom='opacity-100 translate-y-0'
-              leaveTo='opacity-0 translate-y-1'
+              <Transition
+                as={Fragment}
+                enter='transition ease-out duration-200'
+                enterFrom='opacity-0 translate-y-1'
+                enterTo='opacity-100 translate-y-0'
+                leave='transition ease-in duration-150'
+                leaveFrom='opacity-100 translate-y-0'
+                leaveTo='opacity-0 translate-y-1'
+              >
+                <Popover.Panel className='absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5'>
+                  <div className='p-4'>
+                    {products.map((item) => (
+                      <div
+                        key={item.name}
+                        className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50'
+                      >
+                        <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white'>
+                          <item.icon
+                            className='h-6 w-6 text-gray-600 group-hover:text-indigo-600'
+                            aria-hidden='true'
+                          />
+                        </div>
+                        <div className='flex-auto'>
+                          <a
+                            href={item.href}
+                            className='block font-semibold text-gray-900'
+                          >
+                            {item.name}
+                            <span className='absolute inset-0' />
+                          </a>
+                          <p className='mt-1 text-gray-600'>
+                            {item.description}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className='grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50'></div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+
+            <a
+              href='#'
+              className='text-sm font-semibold leading-6 text-gray-900'
             >
-              <Popover.Panel className='absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5'>
-                <div className='p-4'>
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50'
-                    >
-                      <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white'>
-                        <item.icon
-                          className='h-6 w-6 text-gray-600 group-hover:text-indigo-600'
-                          aria-hidden='true'
-                        />
-                      </div>
-                      <div className='flex-auto'>
-                        <a
-                          href={item.href}
-                          className='block font-semibold text-gray-900'
-                        >
-                          {item.name}
-                          <span className='absolute inset-0' />
-                        </a>
-                        <p className='mt-1 text-gray-600'>{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className='grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50'></div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-            Features
-          </a>
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-            Marketplace
-          </a>
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-            Company
-          </a>
-        </Popover.Group>
-        <div className='hidden lg:flex lg:flex-1 lg:justify-end'></div>
-      </nav>
-    </header>
+              Features
+            </a>
+            <a
+              href='#'
+              className='text-sm font-semibold leading-6 text-gray-900'
+            >
+              Marketplace
+            </a>
+            <a
+              href='#'
+              className='text-sm font-semibold leading-6 text-gray-900'
+            >
+              Company
+            </a>
+          </Popover.Group>
+          <div className='hidden lg:flex lg:flex-1 lg:justify-end'></div>
+        </nav>
+      </header>
+      <ProductionGrid />
+    </>
   );
 }
